d6b96f074848d3d095a946dc15b3e5cbb5f8ff9d
Jouke van der Maas <j.maas@niposoftware.com>
2018-12-05 10:30:52 +0100

    Implement first version (iterations board)

--- copy-id.js
+++ copy-id.js
@@ -1,4 +1,21 @@
 (() => {
-  
+  const tileIdRegex = /tile\-(\d+)/
+
+  window.addEventListener('keydown', (event) => {
+    if (event.code === 'KeyC' && event.ctrlKey) {
+      // user pressed Ctrl+C, so we find the card they hover
+      let cards = document.querySelectorAll('.tbTile:hover');
+
+      if (!cards || !cards.length) {
+        return;
+      }
+
+      // there can only be one
+      let hoveredCard = cards[0];
+      let [_, id] = hoveredCard.id.match(tileIdRegex);
+
+      navigator.clipboard.writeText(id);
+    }
+  });
 })()
 
